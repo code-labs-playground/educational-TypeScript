0816d893fbf6f2dd7df1ed74a0d5a8ea534a3346
Lahiru Liyanage <lhlahiru95@gmail.com>
2025-06-01 14:18:49 +0530

    Adding Generics to the types

--- defining-more-complex-types/src/demo.ts
+++ defining-more-complex-types/src/demo.ts
@@ -1,46 +1,25 @@
 type ContactName = string;
+type ContactStatus = "active" | "inactive" | "new"
+type ContactBirthDate = Date | number | string
 
-type ContactStatus = "active" | "inactive" | "new";
-
-type ContactBirthDate = Date | number | string;
-
-interface Contact extends Address {
+interface Contact  {
     id: number;
     name: ContactName;
-    // birthDate?: Date | number | string;
     birthDate?: ContactBirthDate;
     status?: ContactStatus;
-}
-
-interface Address {
-    line1: string;
-    line2: string;
-    province: string;
-    region: string;
-    postalCode: string;
-}
-
-type AddressableContact = Contact & Address;
-
-function getBirthDate(contact: Contact) {
-    if (typeof contact.birthDate === "number") {
-        return new Date(contact.birthDate);
-    }
-    else if (typeof contact.birthDate === "string") {
-        return Date.parse(contact.birthDate)
-    }
-    else {
-        return contact.birthDate
-    }
+    email?: string;
 }
 
 let primaryContact: Contact = {
     id: 12345,
     name: "Jamie Johnson",
-    status: "active",
-    line1: "",
-    line2: "",
-    province: "",
-    region: "",
-    postalCode: ""
+    status: "active"
 }
+
+type ContactFields = keyof Contact;
+
+function getValue<T, U extends keyof T>(source: T, propertyName: U) {
+    return source[propertyName]
+}
+
+const value = getValue({ min: 1, max: 200 }, "")
